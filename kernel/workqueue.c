1f6b1e374d881ea7c6f51b1f100dbe0498bbc180
venkatkamesh <venkat.kamesh90@gmail.com>
2015-06-27 23:18:20 +0530

    Typo : lol build error :p

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -2968,7 +2968,7 @@ struct workqueue_struct *__alloc_workqueue_key(const char *fmt,
 	unsigned int cpu;
 	size_t namelen;
 
-	/* see the comment above the definition of WQ_POWER_EFFICIENT /
+	/* see the comment above the definition of WQ_POWER_EFFICIENT */
 	if ((flags & WQ_POWER_EFFICIENT) && wq_power_efficient)
 		flags |= WQ_UNBOUND;
 
